c19ca7450b787abadcb38e03ec945c9ade2df944
Sonia <lee30sonia@gmail.com>
2021-11-18 15:15:41 +0100

    skeleton

---
+++ include/mockturtle/algorithms/aig_algebraic_rewriting.hpp
@@ -0,0 +1,51 @@
+/*!
+  \file aig_algebraic_rewriting.hpp
+  \brief AIG algebraric rewriting
+
+  EPFL CS-472 2021 Final Project Option 1
+*/
+
+#pragma once
+
+#include "../networks/aig.hpp"
+#include "../views/depth_view.hpp"
+
+namespace mockturtle
+{
+
+namespace detail
+{
+
+template<class Ntk>
+class aig_algebraic_depth_rewriting_impl
+{
+public:
+  aig_algebraic_depth_rewriting_impl( Ntk& ntk )
+    : ntk( ntk )
+  {}
+
+  void run()
+  {
+    
+  }
+
+private:
+
+private:
+  Ntk& ntk;
+};
+
+} // namespace detail
+
+/* Entry point for users to call */
+template<class Ntk>
+void aig_algebraic_depth_rewriting( Ntk& ntk )
+{
+  static_assert( std::is_same_v<typename Ntk::base_type, aig_network>, "Ntk is not an AIG" );
+
+  depth_view dntk{ntk};
+  detail::aig_algebraic_depth_rewriting_impl<Ntk> p( dntk );
+  p.run();
+}
+
+} /* namespace mockturtle */
