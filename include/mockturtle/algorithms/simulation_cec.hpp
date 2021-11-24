869611cc46a9ec15f0ee7fd6500372c61baf7793
aletempiac <alessandro.tempia@gmail.com>
2021-11-24 11:39:52 +0100

    Added cec skeleton and tests

---
+++ include/mockturtle/algorithms/simulation_cec.hpp
@@ -0,0 +1,140 @@
+/* mockturtle: C++ logic network library
+ * Copyright (C) 2018-2021  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file simulation_cec.hpp
+  \brief Simulation-based CEC
+
+  EPFL CS-472 2021 Final Project Option 2
+*/
+
+#pragma once
+
+#include <kitty/constructors.hpp>
+#include <kitty/dynamic_truth_table.hpp>
+#include <kitty/operations.hpp>
+
+#include "../utils/node_map.hpp"
+#include "miter.hpp"
+#include "simulation.hpp"
+
+namespace mockturtle
+{
+
+/* Statistics to be reported */
+struct simulation_cec_stats
+{
+  /*! \brief Split variable (simulation size). */
+  uint32_t split_var{ 0 };
+
+  /*! \brief Number of simulation rounds. */
+  uint32_t rounds{ 0 };
+
+  /*! \brief Simulation error. */
+  bool cec_error{ false };
+};
+
+namespace detail
+{
+
+template<class Ntk>
+class simulation_cec_impl
+{
+public:
+  using pattern_t = unordered_node_map<kitty::dynamic_truth_table, Ntk>;
+  using node = typename Ntk::node;
+  using signal = typename Ntk::signal;
+
+public:
+  explicit simulation_cec_impl( Ntk& ntk, simulation_cec_stats& st )
+      : _ntk( ntk ),
+        _st( st )
+  {
+  }
+
+  bool run()
+  {
+    /* TODO: write your implementation here */
+    return false;
+  }
+
+private:
+  /* you can add additional methods here */
+
+private:
+  Ntk& _ntk;
+  simulation_cec_stats& _st;
+  /* you can add other attributes here */
+};
+
+} // namespace detail
+
+/* Entry point for users to call */
+
+/*! \brief Simulation-based CEC.
+ *
+ * This function implements a simulation-based combinational equivalence checker.
+ * The implementation creates a miter network and run several rounds of simulation
+ * to verify the functional equivalence. For memory and speed reasons this approach
+ * is limited up to 40 input networks.
+ */
+template<class Ntk>
+bool simulation_cec( Ntk const& ntk1, Ntk const& ntk2, simulation_cec_stats* pst = nullptr )
+{
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
+  static_assert( has_size_v<Ntk>, "Ntk does not implement the size method" );
+  static_assert( has_get_node_v<Ntk>, "Ntk does not implement the get_node method" );
+  static_assert( has_foreach_pi_v<Ntk>, "Ntk does not implement the foreach_pi method" );
+  static_assert( has_foreach_po_v<Ntk>, "Ntk does not implement the foreach_po method" );
+  static_assert( has_foreach_node_v<Ntk>, "Ntk does not implement the foreach_node method" );
+  static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
+
+  simulation_cec_stats st;
+
+  bool res = false;
+
+  if ( ntk1.num_pis() <= 40 )
+  {
+    auto ntk_miter = miter<Ntk>( ntk1, ntk2 );
+
+    if ( ntk_miter.has_value() )
+    {
+      detail::simulation_cec_impl p( *ntk_miter, st );
+      res = p.run();
+    }
+  }
+  else
+  {
+    st.cec_error = true;
+  }
+
+  if ( pst )
+    *pst = st;
+
+  return res;
+}
+
+} // namespace mockturtle
---
+++ test/algorithms/simulation_cec.cpp
@@ -0,0 +1,197 @@
+#include <catch.hpp>
+
+#include <mockturtle/algorithms/mapper.hpp>
+#include <mockturtle/algorithms/node_resynthesis/mig_npn.hpp>
+#include <mockturtle/algorithms/node_resynthesis/xag_npn.hpp>
+#include <mockturtle/algorithms/simulation_cec.hpp>
+#include <mockturtle/io/aiger_reader.hpp>
+#include <mockturtle/networks/aig.hpp>
+#include <mockturtle/networks/mig.hpp>
+#include <mockturtle/networks/xag.hpp>
+
+#include <fmt/format.h>
+#include <lorina/aiger.hpp>
+
+using namespace mockturtle;
+
+TEST_CASE( "CEC simple AIG", "[cec]" )
+{
+  aig_network aig1;
+
+  const auto a1 = aig1.create_pi();
+  const auto b1 = aig1.create_pi();
+  const auto c1 = aig1.create_pi();
+  const auto f11 = aig1.create_and( a1, b1 );
+  const auto f21 = aig1.create_and( c1, f11 );
+  aig1.create_po( f21 );
+
+  aig_network aig2;
+
+  const auto a2 = aig2.create_pi();
+  const auto b2 = aig2.create_pi();
+  const auto c2 = aig2.create_pi();
+  const auto f12 = aig2.create_and( b2, c2 );
+  const auto f22 = aig2.create_and( a2, f12 );
+  aig2.create_po( f22 );
+
+  simulation_cec_stats st;
+  CHECK( simulation_cec( aig1, aig2, &st ) );
+  CHECK( st.split_var == 3 );
+  CHECK( st.rounds == 1 );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC different #PIs", "[cec]" )
+{
+  xag_network xag1;
+
+  const auto a1 = xag1.create_pi();
+  const auto b1 = xag1.create_pi();
+  const auto c1 = xag1.create_pi();
+  const auto d1 = xag1.create_pi();
+  const auto f11 = xag1.create_and( a1, b1 );
+  const auto f21 = xag1.create_and( c1, f11 );
+  const auto f31 = xag1.create_and( d1, f21 );
+  const auto f41 = xag1.create_xor( f31, f21 );
+  xag1.create_po( f41 );
+
+  xag_network xag2;
+
+  const auto a2 = xag2.create_pi();
+  const auto b2 = xag2.create_pi();
+  const auto c2 = xag2.create_pi();
+  const auto f12 = xag2.create_and( b2, c2 );
+  const auto f22 = xag2.create_and( a2, f12 );
+  xag2.create_po( f22 );
+
+  simulation_cec_stats st;
+  CHECK( !simulation_cec( xag1, xag2, &st ) );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC different #POs", "[cec]" )
+{
+  aig_network aig1;
+
+  const auto a1 = aig1.create_pi();
+  const auto b1 = aig1.create_pi();
+  const auto c1 = aig1.create_pi();
+  const auto d1 = aig1.create_pi();
+  const auto f11 = aig1.create_and( a1, b1 );
+  const auto f21 = aig1.create_and( c1, f11 );
+  const auto f31 = aig1.create_and( d1, f21 );
+  aig1.create_po( f21 );
+  aig1.create_po( f31 );
+
+  aig_network aig2;
+
+  const auto a2 = aig2.create_pi();
+  const auto b2 = aig2.create_pi();
+  const auto c2 = aig2.create_pi();
+  const auto f12 = aig2.create_and( b2, c2 );
+  const auto f22 = aig2.create_and( a2, f12 );
+  aig2.create_po( f22 );
+
+  simulation_cec_stats st;
+  CHECK( !simulation_cec( aig1, aig2, &st ) );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC too many PIs", "[cec]" )
+{
+  aig_network aig1;
+
+  auto const result = lorina::read_aiger( fmt::format( "{}/adder.aig", BENCHMARKS_PATH ), aiger_reader( aig1 ) );
+  CHECK( result == lorina::return_code::success );
+
+  simulation_cec_stats st;
+  CHECK( !simulation_cec( aig1, aig1, &st ) );
+  CHECK( st.cec_error == true );
+}
+
+TEST_CASE( "CEC different AIGs", "[cec]" )
+{
+  aig_network aig1;
+
+  const auto a1 = aig1.create_pi();
+  const auto b1 = aig1.create_pi();
+  const auto f1 = aig1.create_or( a1, b1 );
+  aig1.create_po( f1 );
+
+  aig_network aig2;
+
+  const auto a2 = aig2.create_pi();
+  const auto b2 = aig2.create_pi();
+  const auto f2 = aig2.create_xor( a2, b2 );
+  aig2.create_po( f2 );
+
+  simulation_cec_stats st;
+  CHECK( !simulation_cec( aig1, aig2, &st ) );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC on optimized design 1", "[cec]" )
+{
+  mig_network mig1;
+
+  auto const result = lorina::read_aiger( fmt::format( "{}/sin.aig", BENCHMARKS_PATH ), aiger_reader( mig1 ) );
+  CHECK( result == lorina::return_code::success );
+
+  mig_npn_resynthesis resyn{ true };
+
+  exact_library<mig_network, mig_npn_resynthesis> lib( resyn );
+
+  mig_network mig2 = map( mig1, lib );
+
+  simulation_cec_stats st;
+  CHECK( simulation_cec( mig1, mig2, &st ) );
+  CHECK( st.split_var == 18 );
+  CHECK( st.rounds == 64 );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC on badly optimized design 1", "[cec]" )
+{
+  mig_network mig1;
+
+  auto const result = lorina::read_aiger( fmt::format( "{}/sin.aig", BENCHMARKS_PATH ), aiger_reader( mig1 ) );
+  CHECK( result == lorina::return_code::success );
+
+  mig_npn_resynthesis resyn{ true };
+
+  exact_library<mig_network, mig_npn_resynthesis> lib( resyn );
+
+  mig_network mig2 = map( mig1, lib );
+
+  auto a = mig2.pi_at( 22 );
+  auto b = mig2.pi_at( 23 );
+  auto f = mig2.create_and( mig2.make_signal( a ), mig2.make_signal( b ) );
+  auto g = mig2.create_or( f, mig2.make_signal( mig2.index_to_node( 4331 ) ) );
+  mig2.replace_in_outputs( mig2.index_to_node( 4331 ), g );
+
+  simulation_cec_stats st;
+  CHECK( !simulation_cec( mig1, mig2, &st ) );
+  CHECK( st.split_var == 18 );
+  CHECK( st.rounds == 64 );
+  CHECK( st.cec_error == false );
+}
+
+TEST_CASE( "CEC on optimized design 2", "[cec]" )
+{
+  xag_network xag1;
+
+  auto const result = lorina::read_aiger( fmt::format( "{}/int2float.aig", BENCHMARKS_PATH ), aiger_reader( xag1 ) );
+  CHECK( result == lorina::return_code::success );
+
+  xag_npn_resynthesis<xag_network> resyn;
+
+  exact_library<xag_network, xag_npn_resynthesis<xag_network>> lib( resyn );
+
+  xag_network xag2 = map( xag1, lib );
+
+  simulation_cec_stats st;
+  CHECK( simulation_cec( xag1, xag2, &st ) );
+  CHECK( st.split_var == 11 );
+  CHECK( st.rounds == 1 );
+  CHECK( st.cec_error == false );
+}
