60091c260c764c0131cee83dc9f57b8d8773471f
Yann Aguettaz <yann@aguettaz.me>
2021-12-06 02:41:47 +0100

    ~

--- include/mockturtle/algorithms/simulation_cec.hpp
+++ include/mockturtle/algorithms/simulation_cec.hpp
@@ -23,12 +23,12 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/*!
-  \file simulation_cec.hpp
-  \brief Simulation-based CEC
+ /*!
+   \file simulation_cec.hpp
+   \brief Simulation-based CEC
 
-  EPFL CS-472 2021 Final Project Option 2
-*/
+   EPFL CS-472 2021 Final Project Option 2
+ */
 
 #pragma once
 
@@ -40,95 +40,150 @@
 #include "miter.hpp"
 #include "simulation.hpp"
 
-namespace mockturtle
-{
+namespace mockturtle {
 
-/* Statistics to be reported */
-struct simulation_cec_stats
-{
-  /*! \brief Split variable (simulation size). */
-  uint32_t split_var{ 0 };
+    /* Statistics to be reported */
+    struct simulation_cec_stats {
+        /*! \brief Split variable (simulation size). */
+        uint32_t split_var{ 0 };
 
-  /*! \brief Number of simulation rounds. */
-  uint32_t rounds{ 0 };
-};
+        /*! \brief Number of simulation rounds. */
+        uint32_t rounds{ 0 };
+    };
 
-namespace detail
-{
+    namespace detail {
+        /*
+            Class similar to default simulator, adapted to our needs
+        */
+        class miter_simulator {
+        public:
+            miter_simulator() = delete;
+            miter_simulator(uint32_t num_vars, uint32_t split_var, uint32_t round) : num_vars(num_vars), split_var(split_var), round(round) {}
 
-template<class Ntk>
-class simulation_cec_impl
-{
-public:
-  using pattern_t = unordered_node_map<kitty::dynamic_truth_table, Ntk>;
-  using node = typename Ntk::node;
-  using signal = typename Ntk::signal;
+            kitty::dynamic_truth_table compute_constant(bool value) const {
+                kitty::dynamic_truth_table tt(split_var);
+                return value ? ~tt : tt;
+            }
 
-public:
-  explicit simulation_cec_impl( Ntk& ntk, simulation_cec_stats& st )
-      : _ntk( ntk ),
-        _st( st )
-  {
-  }
+            kitty::dynamic_truth_table compute_pi(uint32_t index) const {
+                kitty::dynamic_truth_table tt(split_var);
 
-  bool run()
-  {
-    /* TODO: write your implementation here */
-    return false;
-  }
+                if (index < split_var)
+                    // Var index below limit : we actually simulate the variable
+                    kitty::create_nth_var(tt, index);
+                else
+                    // Else the variable is fixed by its corresponding bit in "round"
+                        // If it is 1, nothing to do : the truth table defaults to the correct one
+                        // If it is 0, we negate the truth table to get the correct one
+                    if (((round >> (index - split_var)) & 1) == 0)
+                        tt = ~tt;
 
-private:
-  /* you can add additional methods here */
+                return tt;
+            }
 
-private:
-  Ntk& _ntk;
-  simulation_cec_stats& _st;
-  /* you can add other attributes here */
-};
+            kitty::dynamic_truth_table compute_not(kitty::dynamic_truth_table const& value) const {
+                return ~value;
+            }
 
-} // namespace detail
+        private:
+            uint32_t num_vars;
+            uint32_t split_var;
+            uint32_t round;
+        };
 
-/* Entry point for users to call */
+        template<class Ntk>
+        class simulation_cec_impl {
+        public:
+            using pattern_t = unordered_node_map<kitty::dynamic_truth_table, Ntk>;
+            using node = typename Ntk::node;
+            using signal = typename Ntk::signal;
 
-/*! \brief Simulation-based CEC.
- *
- * This function implements a simulation-based combinational equivalence checker.
- * The implementation creates a miter network and run several rounds of simulation
- * to verify the functional equivalence. For memory and speed reasons this approach
- * is limited up to 40 input networks. It returns an optional which is `nullopt`,
- * if the network has more than 40 inputs.
- */
-template<class Ntk>
-std::optional<bool> simulation_cec( Ntk const& ntk1, Ntk const& ntk2, simulation_cec_stats* pst = nullptr )
-{
-  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
-  static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
-  static_assert( has_size_v<Ntk>, "Ntk does not implement the size method" );
-  static_assert( has_get_node_v<Ntk>, "Ntk does not implement the get_node method" );
-  static_assert( has_foreach_pi_v<Ntk>, "Ntk does not implement the foreach_pi method" );
-  static_assert( has_foreach_po_v<Ntk>, "Ntk does not implement the foreach_po method" );
-  static_assert( has_foreach_node_v<Ntk>, "Ntk does not implement the foreach_node method" );
-  static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
+        public:
+            explicit simulation_cec_impl(Ntk& ntk, simulation_cec_stats& st) : _ntk(ntk), _st(st) {}
 
-  simulation_cec_stats st;
+            bool run() {
+                uint32_t num_pis = _ntk.num_pis();
+                uint32_t num_nodes = _ntk.size();
 
-  bool result = false;
+                // Compute split var and nb of rounds, store it in stats
+                _st.split_var = split_var(num_pis, num_nodes);
+                _st.rounds = 1 << (num_pis - _st.split_var);
 
-  if ( ntk1.num_pis() > 40 )
-    return std::nullopt;
+                // For every round, consider the binary value of "round" as the truth array of the fixed variables
+                for (uint32_t round = 0; round < _st.rounds; round++) {
+                    // Create an instance of our simulator and simulate it
+                    miter_simulator sim(num_pis, _st.split_var, round);
+                    const auto tts = simulate<kitty::dynamic_truth_table>(_ntk, sim);
 
-  auto ntk_miter = miter<Ntk>( ntk1, ntk2 );
+                    // Check if it is constantly 0
+                    for (auto& po : tts)
+                        if (!kitty::is_const0(po))
+                            return false;
+                }
 
-  if ( ntk_miter.has_value() )
-  {
-    detail::simulation_cec_impl p( *ntk_miter, st );
-    result = p.run();
-  }
+                return true;
+            }
 
-  if ( pst )
-    *pst = st;
+        private:
+            /*
+                Determines the split var limit
 
-  return result;
-}
+                @param num_pis   Number of inputs of the circuit
+                @param num_nodes Number of nodes in the circuit
+            */
+            uint32_t split_var(uint32_t num_pis, uint32_t num_nodes) {
+                if (num_pis <= 6) return num_pis;
+
+                for (uint32_t m = 7; ; m++)
+                    if (m == num_pis || (32 + (1 << ((m + 1) - 3))) * num_nodes > (1 << 29))
+                        return m;
+            }
+
+        private:
+            Ntk& _ntk;
+            simulation_cec_stats& _st;
+        };
+    } // namespace detail
+
+    /* Entry point for users to call */
+
+    /*! \brief Simulation-based CEC.
+     *
+     * This function implements a simulation-based combinational equivalence checker.
+     * The implementation creates a miter network and run several rounds of simulation
+     * to verify the functional equivalence. For memory and speed reasons this approach
+     * is limited up to 40 input networks. It returns an optional which is `nullopt`,
+     * if the network has more than 40 inputs.
+     */
+    template<class Ntk>
+    std::optional<bool> simulation_cec(Ntk const& ntk1, Ntk const& ntk2, simulation_cec_stats* pst = nullptr) {
+        static_assert(is_network_type_v<Ntk>, "Ntk is not a network type");
+        static_assert(has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method");
+        static_assert(has_size_v<Ntk>, "Ntk does not implement the size method");
+        static_assert(has_get_node_v<Ntk>, "Ntk does not implement the get_node method");
+        static_assert(has_foreach_pi_v<Ntk>, "Ntk does not implement the foreach_pi method");
+        static_assert(has_foreach_po_v<Ntk>, "Ntk does not implement the foreach_po method");
+        static_assert(has_foreach_node_v<Ntk>, "Ntk does not implement the foreach_node method");
+        static_assert(has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method");
+
+        simulation_cec_stats st;
+
+        bool result = false;
+
+        if (ntk1.num_pis() > 40)
+            return std::nullopt;
+
+        auto ntk_miter = miter<Ntk>(ntk1, ntk2);
+
+        if (ntk_miter.has_value()) {
+            detail::simulation_cec_impl p(*ntk_miter, st);
+            result = p.run();
+        }
+
+        if (pst)
+            *pst = st;
+
+        return result;
+    }
 
 } // namespace mockturtle
